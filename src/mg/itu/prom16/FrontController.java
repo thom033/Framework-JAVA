3fd59081cab87b67abc5a66c440d576e5447ea4a
thom033 <Thomis.rasolomandimby@gmail.com>
2024-05-24 20:04:51 +0300

    commit

--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -27,7 +27,49 @@ public class FrontController extends HttpServlet {
         findClassesInDirectory(controllerPackage, directory);
         checked = true;
     }
-    
+
+    private void findClassesInDirectory(String packageName, File directory) {
+        for (File file : Objects.requireNonNull(directory.listFiles())) {
+            if (file.isDirectory()) {
+                findClassesInDirectory(packageName + "." + file.getName(), file);
+            } else if (file.getName().endsWith(".class")) {
+                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                addClassIfController(className);
+            }
+        }
+    }
+
+    protected void processRequested(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        try {
+            String url = req.getRequestURL().toString();
+            out.println("URL: " + url);
+   
+            if (!checked) {
+                findControllerClasses();
+            }
+
+            out.println("Liste des classes controleurs :");
+            for (Class<?> controllerClass : controllerClasses) {
+                out.println(controllerClass.getName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    private void addClassIfController(String className) {
+        try {
+            Class<?> clazz = Class.forName(className);
+            if (clazz.isAnnotationPresent(AnnotationController.class)) {
+                controllerClasses.add(clazz);
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Class not found: " + className);
+        }
+    }
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -61,5 +103,13 @@ public class FrontController extends HttpServlet {
             PrintWriter out = response.getWriter();
             out.println("Erreur: " + ex.getMessage());
         }
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequested(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequested(req, res);
     }
 }
