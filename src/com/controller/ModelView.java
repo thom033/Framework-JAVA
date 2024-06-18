d294fdb504a4f57db1acbeabc8f6f24e8c84b28f
thom033 <Thomis.rasolomandimby@gmail.com>
2024-06-18 23:21:23 +0300

    nanampy

---
+++ src/com/controller/ModelView.java
@@ -0,0 +1,37 @@
+package com.controller;
+
+import java.util.HashMap;
+
+public class ModelView {
+    private String url;
+    private HashMap <String, Object> data = new HashMap<>();;
+
+    public ModelView(String url, HashMap<String, Object> data) {
+        setUrl(url);
+        setData(data);
+    }
+
+    public ModelView(String url) {
+        setUrl(url);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public HashMap <String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
+
+    public void addData (String nom, Object value){
+        this.data.put(nom,value);
+    }
+}
