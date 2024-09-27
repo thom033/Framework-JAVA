8fe9362f2abced437658f17ea12cacfe3cdaacb0
thom033 <Thomis.rasolomandimby@gmail.com>
2024-09-27 14:55:32 +0300

    Sprint 9 commit 1

--- jar.bat
+++ jar.bat
@@ -3,6 +3,7 @@ setlocal
 
 REM Set the servlet API path
 set "SERVLET_API=%cd%\lib\servlet-api.jar"
+set "GSON_API=%cd%\lib\gson-2.10.jar"
 
 REM Set the source directory
 set "src=%cd%\src"
@@ -13,7 +14,7 @@ set "JAR_FILE=myLib\framework.jar"
 
 REM Compile all .java files in the source directory
 for /R "%src%" %%f in (*.java) do (
-    javac -cp "%SERVLET_API%;%src%" -d "%OUTPUT_DIR%" "%%f"
+    javac -cp "%SERVLET_API%;%GSON_API%;%src%" -d "%OUTPUT_DIR%" "%%f"
 )
 
 REM Change directory to the output directory
@@ -23,7 +24,7 @@ REM Create the JAR file
 jar cvf "%JAR_FILE%" *
 
 REM Copy the JAR file to the target directory
-copy "%JAR_FILE%" "E:\S4\Web-Dynamique\GitLab\Sprint0Test\lib"
+copy "%JAR_FILE%" "F:\S4\Sprint0Test\lib"
 
 echo JAR creation and copy completed.
 pause
